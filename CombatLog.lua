75d842d367f5332258e20d61191eecf4051ce6a8
Mikael Hultgren <blomma@fastmail.fm>
2009-01-26 19:00:58 +0100

    Made use of PLAYER_ENTERING_WORLD instead, also added some output.

--- CombatLog.lua
+++ CombatLog.lua
@@ -1,12 +1,18 @@
 ﻿local IsInInstance = IsInInstance
 
 local f = CreateFrame("Frame")
-f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
 f:SetScript("OnEvent", function(frame, arg1)
 	local inInstance, instanceType = IsInInstance()
 	if( inInstance and instanceType == "raid" ) then
 		LoggingCombat(1)
 	else
 		LoggingCombat(0)
+	end
+	
+	if LoggingCombat() then
+		DEFAULT_CHAT_FRAME:AddMessage( "Combat log enabled" )
+	else
+		DEFAULT_CHAT_FRAME:AddMessage( "Combat log disabled" )
 	end
 end)
