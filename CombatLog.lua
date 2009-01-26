ed806493213a2e6cc4832fc7c151e53aecd2ff05
Mikael Hultgren <blomma@fastmail.fm>
2009-01-26 06:40:51 +0100

    Inital commit.

---
+++ CombatLog.lua
@@ -0,0 +1,12 @@
+﻿local IsInInstance = IsInInstance
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("ZONE_CHANGED_NEW_AREA")
+f:SetScript("OnEvent", function(frame, arg1)
+	local inInstance, instanceType = IsInInstance()
+	if( inInstance and instanceType == "raid" ) then
+		LoggingCombat(1)
+	else
+		LoggingCombat(0)
+	end
+end)
